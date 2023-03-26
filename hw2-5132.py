53860df215abec4db299ffa447793e84df1d56f2
anusorn <anusorn.l@somapait.com>
2023-03-26 23:17:12 +0700

    fix(-): แก้ไขมา run บน jupyter ด้วย AWS sageMaker

--- hw2-5132.py
+++ hw2-5132.py
@@ -63,11 +63,11 @@ def list_bucket_contents(bucket, match='', size_mb=0):
     
     print(f'Bucket {bucket} total size is {total_size_gb/1024:3.1f}GB with {total_files} files')
 
-print('############### list_bucket_contents ')
 list_bucket_contents(bucket='nyc-tlc', match='2017', size_mb=250)
 
-# Preview CSV Dataset
-def preview_csv_dataset(bucket, key, rows=10):
+# pip install pyarrow
+# Preview Dataset
+def preview_dataset(bucket, key):
     data_source = {
             'Bucket': bucket,
             'Key': key
@@ -82,10 +82,9 @@ def preview_csv_dataset(bucket, key, rows=10):
     data = pd.read_parquet(url, engine='pyarrow')
     return data
 
-# Preview CSV Dataset
-print('############### preview dataset')
-df = preview_csv_dataset(bucket='nyc-tlc', key='trip data/green_tripdata_2017-01.parquet', rows=100)
-df.head()
+df = preview_dataset(bucket='nyc-tlc', key=f'trip data/yellow_tripdata_2017-01.parquet')
+df.head(5)
+
 df.shape
 df.info()
 df.describe()
@@ -113,43 +112,262 @@ def copy_among_buckets(from_bucket, from_key, to_bucket, to_key):
     else:
         print(f'File {to_key} already exists in S3 bucket {to_bucket}') 
 
-print('############### Copy Among Buckets')
-copy_among_buckets(from_bucket='nyc-tlc', from_key='trip data/yellow_tripdata_2017-01.parquet',
-                      to_bucket='nyc-tlc-cs653-5132', to_key='few-trips/trips-2017-01.parquet')
-copy_among_buckets(from_bucket='nyc-tlc', from_key='trip data/yellow_tripdata_2017-02.parquet',
-                      to_bucket='nyc-tlc-cs653-5132', to_key='few-trips/trips-2017-02.parquet')
-copy_among_buckets(from_bucket='nyc-tlc', from_key='trip data/yellow_tripdata_2017-03.parquet',
-                      to_bucket='nyc-tlc-cs653-5132', to_key='few-trips/trips-2017-03.parquet')
-copy_among_buckets(from_bucket='nyc-tlc', from_key='trip data/green_tripdata_2017-01.parquet',
-                      to_bucket='nyc-tlc-cs653-5132', to_key='few-trips/green_tripdata_2017-01.parquet')
+for i in range(1, 6):
+    copy_among_buckets(from_bucket='nyc-tlc', from_key=f'trip data/yellow_tripdata_2017-0{i}.parquet',
+                      to_bucket='nyc-tlc-cs653-5132', to_key=f'yellow_tripdata-2017-0{i}.parquet')
+    
+#/////////////////////////////////// Answer a /////////////////////////////////////////
+import boto3
 
-# Set up S3 Select parameters
-query1 ="select payment_type, count(payment_type) from s3object s group by s.payment_type"
-query2 ="select PULocationID, count(PULocationID) as rides, sum(total_amount) as 'Total amount', avg(passenger_count) as 'AVG passenger count' from s3object s group by s.PULocationID"
-query3 =""
+s3 = boto3.client('s3')
+
+# Answer a
 bucket = 'nyc-tlc-cs653-5132'
-key = 'few-trips/trips-2017-01.parquet'
+key = 'yellow_tripdata-2017-01.parquet'
 expression_type = 'SQL'
 input_serialization = {'Parquet': {}}
 output_serialization = {'CSV': {}}
 
-# Execute S3 Select query
-def s3_select(bucket, key, query):
+def creditCard():
+    return "Credit card"
+def cash():
+    return "Cash"
+def noCharge():
+    return "No charge"
+def dispute():
+    return "Dispute"
+def unknow():
+    return "Unknow"
+def voidedTrip():
+    return "Voided trip"
+def default():
+    return "Incorrect payment type"
+
+switcher = {
+    1: creditCard,
+    2: cash,
+    3: noCharge,
+    4: dispute,
+    5: unknow,
+    6: voidedTrip
+    }
+
+def switch(payment_type):
+    return switcher.get(payment_type, default)()
+
+sum = 0
+for i in range(1, 6):
+    # Execute S3 Select query
     response = s3.select_object_content(
         Bucket=bucket,
         Key=key,
-        Expression=query,
+        Expression=f"select count(payment_type) from s3object s where payment_type = {i}",
         ExpressionType=expression_type,
         InputSerialization=input_serialization,
         OutputSerialization=output_serialization,
     )
-
+    
     # Iterate through the response and print each line
     for event in response['Payload']:
         if 'Records' in event:
             records = event['Records']['Payload'].decode('utf-8')
-            print(records)
+            sum = sum + int(records)
+            print(f"ประเภทการจ่ายเงิน {switch(i)} มีจำนวน yellow taxi rides เท่ากับ {records}")
+print(f"มีจำนวน yellow taxi rides ทั้งหมดเท่ากับ {sum}") 
 
-s3_select(bucket, key, query1)
-s3_select(bucket, key, query2)
-s3_select(bucket, key, query3)
+#/////////////////////////////////// Answer b /////////////////////////////////////////
+import numpy as np
+PUL_ID = df['PULocationID'].unique()
+np.sort(PUL_ID)
+
+import boto3
+
+s3 = boto3.client('s3')
+
+# Answer a
+bucket = 'nyc-tlc-cs653-5132'
+key = 'yellow_tripdata-2017-01.parquet'
+expression_type = 'SQL'
+input_serialization = {'Parquet': {}}
+output_serialization = {'CSV': {}}
+
+# คำนวนค่าโดยสารรวมของ rides ในแต่ละจุด
+def sum_total_amount(pulid):
+    response = s3.select_object_content(
+        Bucket=bucket,
+        Key=key,
+        Expression=f"select sum(total_amount)  from s3object s where PULocationID = {i}",
+        ExpressionType=expression_type,
+        InputSerialization=input_serialization,
+        OutputSerialization=output_serialization,
+    )
+    
+    # Iterate through the response and print each line
+    for event in response['Payload']:
+        if 'Records' in event:
+            records = event['Records']['Payload'].decode('utf-8')
+            
+            try:
+                isinstance(float(records), float)
+                return float(records)
+            except:
+                return None
+
+# จำนวนผู้โดยสารเฉลี่ยต่อ rides ในแต่ละจุด
+def avg_passenger_count(pulid):
+    response = s3.select_object_content(
+        Bucket=bucket,
+        Key=key,
+        Expression=f"select avg(passenger_count) from s3object s where PULocationID = {i}",
+        ExpressionType=expression_type,
+        InputSerialization=input_serialization,
+        OutputSerialization=output_serialization,
+    )
+    
+    # Iterate through the response and print each line
+    for event in response['Payload']:
+        if 'Records' in event:
+            records = event['Records']['Payload'].decode('utf-8')
+            
+            try:
+                isinstance(float(records), float)
+                return float(records)
+            except:
+                return None            
+
+# Define 3 array, keep to show in DataFrame
+pul_id_arr=[]
+total_fare_arr=[]
+avg_pass_arr=[]
+
+for i in range(1, 266):
+    # Execute S3 Select query
+    response = s3.select_object_content(
+        Bucket=bucket,
+        Key=key,
+        Expression=f"select count(PULocationID) from s3object s where PULocationID = {i}",
+        ExpressionType=expression_type,
+        InputSerialization=input_serialization,
+        OutputSerialization=output_serialization,
+    )
+    
+    # Iterate through the response and print each line
+    for event in response['Payload']:
+        if 'Records' in event:
+            records = event['Records']['Payload'].decode('utf-8')
+            
+            pul_id_arr.append(i)
+            print(f"จุดรับผู้โดยสารที่ {i} มีจำนวน Yellow taxi rides เท่ากับ {int(records)}")
+            
+            total_fare = sum_total_amount(i)
+            if isinstance(total_fare, float):
+                total_fare = float("{:.2f}".format(total_fare))
+                total_fare_arr.append(total_fare)
+                print(f"ค่าโดยสารรวม {total_fare} $")
+            else:
+                total_fare = 0.0
+                total_fare_arr.append(total_fare)
+                print(f"ค่าโดยสารรวม - $")
+                
+            avg_pass = avg_passenger_count(i)
+            if isinstance(avg_pass, float):
+                avg_pass = float("{:.2f}".format(avg_pass))
+                avg_pass_arr.append(avg_pass)
+                print(f"จำนวนผู้โดยสารเฉลี่ยต่อ rides เท่ากับ {avg_pass} คน")
+            else:
+                avg_pass = 0.0
+                avg_pass_arr.append(avg_pass)
+                print(f"จำนวนผู้โดยสารเฉลี่ยต่อ rides เท่ากับ  - คน")
+            print(f"-----------------------------------------")
+
+label_data = {'จุดรับ': pul_id_arr, 
+              'ค่าโดยสารรวม': total_fare_arr, 
+              'จำนวนผู้โดยสารเฉลี่ยต่อ rides': avg_pass_arr}
+dfm = pd.DataFrame(label_data)
+dfm
+
+#/////////////////////////////////// Answer c /////////////////////////////////////////
+import boto3
+
+s3 = boto3.client('s3')
+
+# Define parameter
+bucket = 'nyc-tlc-cs653-5132'
+expression_type = 'SQL'
+input_serialization = {'Parquet': {}}
+output_serialization = {'CSV': {}}
+
+# Define 6 array of payment type, keep to show in DataFrame
+cc_arr = []
+c_arr = []
+nc_arr = []
+dp_arr = []
+uk_arr = []
+vt_arr = []
+summary = []
+
+def creditCard():
+    cc_arr.append(records)
+    return "Credit card"
+def cash():
+    c_arr.append(records)
+    return "Cash"
+def noCharge():
+    nc_arr.append(records)
+    return "No charge"
+def dispute():
+    dp_arr.append(records)
+    return "Dispute"
+def unknow():
+    uk_arr.append(records)
+    return "Unknow"
+def voidedTrip():
+    vt_arr.append(records)
+    return "Voided trip"
+def default():
+    print("Incorrect payment type")
+
+switcher = {
+    1: creditCard,
+    2: cash,
+    3: noCharge,
+    4: dispute,
+    5: unknow,
+    6: voidedTrip
+    }
+
+def switch(payment_type):
+    return switcher.get(payment_type, default)()
+
+def sum_rides_by_month(month):
+    sum = 0
+    for i in range(1, 6):
+        # Execute S3 Select query for answer a
+        response = s3.select_object_content(
+            Bucket=bucket,
+            Key=f'yellow_tripdata-2017-0{month}.parquet',
+            Expression=f"select count(payment_type) from s3object s where payment_type = {i}",
+            ExpressionType=expression_type,
+            InputSerialization=input_serialization,
+            OutputSerialization=output_serialization,
+        )
+    
+        # Iterate through the response and print each line
+        for event in response['Payload']:
+            if 'Records' in event:
+                records = event['Records']['Payload'].decode('utf-8')
+
+                try:
+                    isinstance(int(records), int)
+                    records = int(records)
+                except:
+                    return None 
+
+                sum = sum + records
+                print(f"ในเดือน {month} ที่ประเภทการจ่ายเงิน {switch(i)} มีจำนวน yellow taxi rides เท่ากับ {records}")
+
+    summary.append(sum)
+    print(f"เดือน {month} มีจำนวน yellow taxi rides รวมทั้งสิ้น {sum} รอบ")
+    print(f"-----------------------------------------")
+
+    
